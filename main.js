087c9248f111834f584a2cbc2506c26cb284b16d
Shawn-Nevermore <shawnxu2016sg@gmail.com>
2019-08-08 11:44:43 +0800

    搭建基本HTML

---
+++ main.js
@@ -0,0 +1,16 @@
+!function(){
+    function writeCode(prefix, code, fn){
+        let code = document.querySelector('#code')
+        let styleTag = document.querySelector()
+        let n = 0
+        let duration = 30
+        
+        let id = setTimeout(function fn(){
+            
+            if
+            setTimeout(function fn(){
+                
+            }, duration);
+        }, duration);
+    }
+}.call()
